c3b0118a27f7c352ab1be04cc023ac99609cd1a5
WoW-projekt <lzirdum1@tvz.hr>
2026-03-28 12:56:31 +0100

    Update PlayerProfile comments and agent log

--- WoWprojekt/WoWprojekt/Models/PlayerProfile.cs
+++ WoWprojekt/WoWprojekt/Models/PlayerProfile.cs
@@ -17,8 +17,8 @@ public class PlayerProfile
 
     public DateTime LastUpdatedAt { get; set; } = DateTime.UtcNow;
 
-    public int? GuildId { get; set; }
-    public Guild? Guild { get; set; }
+    public int? GuildId { get; set; } //uzima se id iz Guild klase, nullable jer igrač možda nije u guildu i sprema kao int
+    public Guild? Guild { get; set; } //isto kao gore samo uzima cijeli guild umjesto samo id-a
 
     public ICollection<TalentBuild> TalentBuilds { get; set; } = new List<TalentBuild>();
     public ICollection<PlayerProfession> Professions { get; set; } = new List<PlayerProfession>();
